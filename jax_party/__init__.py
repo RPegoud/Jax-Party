6c4400eae5943223dc5176684264b82aec199cbb
RPegoud <ryanpegoud@gmail.com>
2025-02-06 20:12:15 +0000

    feat: abstracted vault boilerplate to custom class

--- jax_party/__init__.py
+++ jax_party/__init__.py
@@ -1,3 +1,4 @@
 from jax_party.env_types import Action, State, Observation
 from jax_party.utils import tree_slice, tree_add_element
 from jax_party.env import JaxParty, PartyGenerator, PartyMARLWrapper, register_JaxParty
+from jax_party.vault import make_buffer_and_vault, PartyVault
--- jax_party/vault.py
+++ jax_party/vault.py
@@ -1,15 +1,25 @@
-from typing import Dict
+from typing import Callable, Dict
+from colorama import Fore, Style
 import flashbax as fbx
 from flashbax.vault import Vault
 import jax
 import jax.numpy as jnp
 import chex
+from omegaconf import OmegaConf
+from jax_party import JaxParty
+from typing import TypeVar
+
+Transition = TypeVar("Transition")
+BufferState = TypeVar("BufferState")
+Experience = TypeVar("Experience")
 
 
 class PartyVault(Vault):
     def __init__(
         self,
         vault_name,
+        save_interval: int,
+        buffer_add: Callable[[BufferState, Experience], BufferState],
         experience_structure=None,
         rel_dir="vaults",
         vault_uid=None,
@@ -24,10 +34,13 @@ class PartyVault(Vault):
             compression,
             metadata,
         )
+        self.save_interval = save_interval
+        self.buffer_add = buffer_add
 
     @jax.jit
     def _reshape_experience(
-        self, experience: Dict[str, chex.Array]
+        self,
+        experience: Dict[str, chex.Array],
     ) -> Dict[str, chex.Array]:
         """Reshape experience to match buffer."""
         # Swap the T and NE axes (D, NU, UB, T, NE, ...) -> (D, NU, UB, NE, T, ...)
@@ -36,4 +49,87 @@ class PartyVault(Vault):
         experience = jax.tree.map(lambda x: x.reshape(-1, *x.shape[4:]), experience)
         return experience
 
-    def _push_vault_to_neptune(): ...
+    def add_and_write(self, experience: Experience, eval_step: int):
+        flashbax_transition = self._reshape_experience(
+            {
+                # (D, NU, UB, T, NE, ...)
+                "done": experience.done,
+                "action": experience.action,
+                "reward": experience.reward,
+                "observation": experience.obs.agents_view,
+                "legal_action_mask": experience.obs.action_mask,
+            }
+        )
+        # Add to fbx buffer
+        buffer_state = self.buffer_add(buffer_state, flashbax_transition)
+
+        # Save buffer into vault
+        if eval_step % self.save_interval == 0:
+            write_length = self.write(buffer_state)
+            print(
+                f"{Fore.WHITE}{Style.BRIGHT}(Wrote {write_length}) Vault index = {self.vault_index}{Style.RESET_ALL}"
+            )
+
+    def push_vault_to_neptune(self):  # TODO:
+        raise NotImplementedError
+
+
+def make_buffer_and_vault(
+    env: JaxParty,
+    config: dict,
+    vault_id: str = None,  # None is converted to timestamp
+) -> tuple[
+    BufferState,
+    PartyVault,
+]:
+    # TODO: convert int32 to int8
+    n_devices = len(jax.devices())
+
+    dummy_flashbax_transition = {
+        "done": jnp.zeros((config.system.num_agents,), dtype=bool),
+        "action": jnp.zeros((config.system.num_agents,), dtype=jnp.int32),
+        "reward": jnp.zeros((config.system.num_agents,), dtype=jnp.float32),
+        "observation": jnp.zeros(
+            (
+                config.system.num_agents,
+                env.observation_spec.agents_view.shape[1],
+            ),
+            dtype=jnp.float32,
+        ),
+        "legal_action_mask": jnp.zeros(
+            (
+                config.system.num_agents,
+                env.num_actions,
+            ),
+            dtype=bool,
+        ),
+    }
+
+    buffer = fbx.make_flat_buffer(
+        max_length=int(1e6),  # Max number of transitions to store
+        min_length=int(1),
+        sample_batch_size=1,
+        add_sequences=True,
+        add_batch_size=(
+            n_devices
+            * config.system.num_updates_per_eval
+            * config.system.update_batch_size
+            * config.arch.num_envs
+        ),
+    )
+    buffer_state = buffer.init(
+        dummy_flashbax_transition,
+    )
+    buffer_add = jax.jit(buffer.add, donate_argnums=(0))
+    vault = PartyVault(
+        vault_name=config.vault.vault_name,
+        experience_structure=buffer_state.experience,
+        vault_uid=vault_id,
+        metadata=OmegaConf.to_container(
+            config, resolve=True
+        ),  # Metadata must be a python dictionary
+        buffer_add=buffer_add,
+        save_interval=config.vault.save_interval,
+    )
+
+    return buffer_state, vault
